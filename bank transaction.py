ed45087a847b7eddcdf890b7c9480a557ae125da
sharmirajesh <48148551+sharmirajesh@users.noreply.github.com>
2019-08-13 13:50:23 +0530

    Create bank transaction.py

---
+++ bank transaction.py
@@ -0,0 +1,18 @@
+net_amount = 0
+
+while True:
+	str = raw_input ("Enter transaction: ")
+  transaction = str.split(" ")
+  type = transaction [0]
+	amount = int (transaction [1])
+
+	if type=="D" or type=="d":
+		net_amount += amount
+	elif type=="W" or type=="w":
+		net_amount -= amount
+	else:
+		pass
+  str = raw_input ("want to continue (Y for yes) : ")
+	if not (str[0] =="Y" or str[0] =="y") :
+		break
+print "Net amount: ", net_amount
